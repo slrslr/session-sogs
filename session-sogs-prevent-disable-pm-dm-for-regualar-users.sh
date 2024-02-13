d5bc295cf562ccdad45a0291d0cd06db8e22e86f
slrslr <6596726+slrslr@users.noreply.github.com>
2024-02-13 20:56:35 +0000

    Create session-sogs-prevent-disable-pm-dm-for-regualar-users.sh

---
+++ session-sogs-prevent-disable-pm-dm-for-regualar-users.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Linux bash script. Make this file executable (chmod +x filename) and run ("./filename" or "bash filename").
+
+read -r -p "Add a database trigger to prevent DM/PM by regular users while allowing the same for admins/mods with upload permission? Type letter a to add trigger or letter r to remove trigger(allowing anyonone to PM):" i
+if [[ "$i" == "r" ]]; then sqlite3 /var/lib/session-open-group-server/sogs.db 'DROP TRIGGER insert_into_inbox;'; else
+
+--() { :; }; exec sqlite3 /var/lib/session-open-group-server/sogs.db <<EOF
+
+CREATE TRIGGER insert_into_inbox
+BEFORE INSERT ON inbox
+FOR EACH ROW
+WHEN (
+NEW.recipient NOT IN (SELECT user FROM user_permissions WHERE admin = 1)
+AND NEW.sender NOT IN (SELECT user FROM user_permissions WHERE admin = 1)
+AND NEW.recipient NOT IN (SELECT user FROM user_permission_overrides WHERE upload=1)
+AND NEW.sender NOT IN (SELECT user FROM user_permission_overrides WHERE upload=1)
+)
+BEGIN
+-- Abort the insert on the inbox table
+SELECT RAISE(FAIL, 'Insert operation not allowed for non-admin users.');
+-- SELECT RAISE(IGNORE);
+END;
+
+.quit
+EOF
+
+fi
+
+echo "No error? Then it should be complete. Try to verify that it works."
