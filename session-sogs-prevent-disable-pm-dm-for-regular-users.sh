2ff8e76317aec086251f5d3f05dd504da03076da
slrslr <6596726+slrslr@users.noreply.github.com>
2024-06-28 05:46:56 +0000

    Update session-sogs-prevent-disable-pm-dm-for-regular-users.sh
    It may allow also visible mods to PM. Based on https://stuff.oddch.at/51397/restrict-non-admin-users-from-sending-dms

--- session-sogs-prevent-disable-pm-dm-for-regular-users.sh
+++ session-sogs-prevent-disable-pm-dm-for-regular-users.sh
@@ -6,18 +6,21 @@ if [[ "$i" == "r" ]]; then sqlite3 /var/lib/session-open-group-server/sogs.db 'D
 
 --() { :; }; exec sqlite3 /var/lib/session-open-group-server/sogs.db <<EOF
 
+-- "TRIGGER: Restrict non-admin users from sending DMs"
 CREATE TRIGGER insert_into_inbox
 BEFORE INSERT ON inbox
 FOR EACH ROW
 WHEN (
-NEW.recipient NOT IN (SELECT user FROM user_permissions WHERE admin = 1)
-AND NEW.sender NOT IN (SELECT user FROM user_permissions WHERE admin = 1)
+-- Allow visible mods to send messages to anyone
+NEW.recipient NOT IN (SELECT user FROM user_permissions WHERE admin = 1 AND visible_mod=1)
+AND NEW.sender NOT IN (SELECT user FROM user_permissions WHERE admin = 1 AND visible_mod=1)
+-- Allow uploaders to send messages to anyone
 AND NEW.recipient NOT IN (SELECT user FROM user_permission_overrides WHERE upload=1)
 AND NEW.sender NOT IN (SELECT user FROM user_permission_overrides WHERE upload=1)
 )
 BEGIN
 -- Abort the insert on the inbox table
-SELECT RAISE(FAIL, 'Insert operation not allowed for non-admin users.');
+SELECT RAISE(FAIL, 'Insert operation not allowed.');
 -- SELECT RAISE(IGNORE);
 END;
 
